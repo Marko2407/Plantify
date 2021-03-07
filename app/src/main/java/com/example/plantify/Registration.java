5c5f99335b902b29db40e395ab95968d898e07c9
Marko Vukosav <marko.vukosav85@gmail.com>
2021-03-07 19:56:59 +0100

    Register view, 1st and 2nd view connected

--- app/src/main/java/com/example/plantify/Registration.java
+++ app/src/main/java/com/example/plantify/Registration.java
@@ -2,10 +2,16 @@ package com.example.plantify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registration extends AppCompatActivity {
 
+    public void btnLogBack (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
